7f8be278b11bc3dacd0cabfe9c601099c5980bfb
chankruze <chankruze@gmail.com>
2022-03-29 17:28:11 +0530

    feat: add css styles
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,9 @@
-import { LiveReload, Outlet } from "remix";
+import { LiveReload, Outlet, Links, LinksFunction } from "remix";
 import type { MetaFunction } from "remix";
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -7,12 +11,32 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
+
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,6 +5,19 @@ Created: Tue Mar 29 2022 16:35:56 GMT+0530 (India Standard Time)
 Copyright (c) geekofia 2022 and beyond
 */
 
+import { LinksFunction } from "remix";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
 export default function IndexRoute() {
   return <div>Root Route</div>;
 }
