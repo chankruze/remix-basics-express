958b2a02d13eb25c951dc710c01902a536093ec3
chankruze <chankruze@gmail.com>
2022-03-29 16:24:50 +0530

    init: bare minimum remix setup
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/root.tsx
@@ -0,0 +1,25 @@
+import {
+  LiveReload,
+} from "remix";
+import type { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
+      </head>
+      <body>
+        Hello world
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
